52aa1aa8422386d5ecf4a47bfdbe0705946348ad
Your Name <you@example.com>
2026-07-08 11:50:56 +0000

    fix: update renew-cert.sh to use system certbot and correct docker-compose

--- renew-cert.sh
+++ renew-cert.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-cd /opt/sora_earth_ai_platform
-docker run --rm -v /opt/sora_earth_ai_platform/certs:/etc/letsencrypt -v /opt/sora_earth_ai_platform/certbot/www:/var/www/certbot certbot/certbot renew --webroot -w /var/www/certbot --quiet
-docker compose -f docker-compose.prod.yml exec -T nginx nginx -s reload
+certbot renew --quiet --deploy-hook "docker compose -f /opt/sora_earth_ai_platform/docker-compose.yml exec -T nginx nginx -s reload"
